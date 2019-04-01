ee24dec86366ba42d32c9d93fe99ca32439b6695
andre.forchesatto <andreforchesatto@gmail.com>
2019-04-01 20:03:48 -0300

    criado conversor generico

--- xmlAula2019/src/main/java/xmlAula2019/Main.java
+++ xmlAula2019/src/main/java/xmlAula2019/Main.java
@@ -10,6 +10,7 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import xmlAula2019.conversor.ConverterXML;
 import xmlAula2019.model.Cidade;
 import xmlAula2019.model.Endereco;
 import xmlAula2019.model.Pessoa;
@@ -18,48 +19,25 @@ import xmlAula2019.model.Uf;
 
 public class Main {
 
-	
 	public static void main(String[] args) {
-		Cidade xanxere = new Cidade("Xanxerê",new Uf("Santa catarina","SC"));
+		Cidade xanxere = new Cidade("Xanxerê", new Uf("Santa catarina", "SC"));
 		Endereco endereco = new Endereco("Rua teste 1", "bairro 1", xanxere);
 		Pessoa pessoa = new Pessoa("André", 87654, endereco);
-		Endereco endereco2 = new Endereco("Rua teste 2", "bairro 2",xanxere);
+		Endereco endereco2 = new Endereco("Rua teste 2", "bairro 2", xanxere);
 		Pessoa pessoa2 = new Pessoa("Joaquim", 87654, endereco2);
-		
+
 		List<Pessoa> pessoas = new ArrayList<>();
 		pessoas.add(pessoa);
 		pessoas.add(pessoa2);
+
+		ConverterXML<Pessoas> conversor = new ConverterXML<>(Pessoa.class, 
+				Pessoas.class, Endereco.class, Cidade.class, Uf.class);
 		
+		conversor.gerarXml(new File("pessoas.xml"), new Pessoas(pessoas));
 		
-		try {
-			JAXBContext jaxbContext = JAXBContext.newInstance(Pessoa.class, 
-					Pessoas.class, Cidade.class, Uf.class, Endereco.class);
-			
-			Marshaller marshaller = jaxbContext.createMarshaller();
-			marshaller.marshal(pessoa, new File("pessoa.xml"));
-			
-			Unmarshaller unMarshaller = jaxbContext.createUnmarshaller();
-			Pessoa pessoaOBJ = 
-					(Pessoa) unMarshaller.unmarshal(new File("pessoa.xml"));
-			
-			System.out.println(pessoaOBJ);
-			
-			Marshaller marshaller2 = jaxbContext.createMarshaller();
-			StringWriter stringWriter = new StringWriter();
-			marshaller2.marshal(pessoa, stringWriter);
-			String xml = stringWriter.toString();
-			System.out.println(xml);
-			
-			Marshaller marshallerPessoas = jaxbContext.createMarshaller();
-			stringWriter = new StringWriter();
-			marshallerPessoas.marshal(new Pessoas(pessoas), stringWriter);
-			xml = stringWriter.toString();
-			System.out.println(xml);
-			
-			
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
+		Pessoas pessoasConv = conversor.gerarObj(new File("pessoas.xml"));
 		
+		System.out.println(pessoasConv);
+
 	}
 }
